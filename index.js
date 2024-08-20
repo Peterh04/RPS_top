eddad20e2dec682c157dd93babad77a40a0964c0
Peter Heri <138527665+Peterh04@users.noreply.github.com>
2024-08-20 19:18:21 +0300

    remove unecessary comments after learning about comments

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
-/*Create a variable of playerName of type string */
-let playerName;
 
-/*Create a variable of number of rounds of type Number */
+let playerName;
 let numberOfRounds;
 
-/*Create array of game choices */
 const gameChoices = ['Rock', 'Paper', 'Scissors']
 
-/*Create a variable of playerChoice of type string */
 let playerChoice;
 
-/*create a variable of computerchoice of type string  with random values of gameChoices assigned to it*/
 let computerchoice;
 
-/*Create a variable of current number of round and assign 1 */
 let currentNumberOfRound;
 
-/*Create a variable for Computer score of type number */
 let computerScore;
 
-/*Create a variable for Player score of type number */
 let playerScore;
 
-
-//Create a variable for restart with yes  assigned to it
 let restart = 'Yes'
 
-// ask the user the name and assign to playerName
 playerName =  prompt('What is your name? ');
 
 
 //outer while loop to control the restart of the game
 while (restart === 'Yes') {
-    //restart necessary variables if game restarted
+    //restarts necessary variables if game restarted
 
     computerScore = 0;
     playerScore = 0;
@@ -88,9 +77,7 @@ while (restart === 'Yes') {
 
 
 
-/*Create a restart game choice array */
 
-/*Create a variable to track restart game choice */
 
 if(playerScore > computerScore){
     console.log('Hurray you win the game!🥳')
