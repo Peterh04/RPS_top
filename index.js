d3e001478285fc43512cf1b87685b32724461da4
Peter Heri <heripeter725@gmail.com>
2024-08-28 16:53:38 +0300

    Made UI

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 
-let playerName;
-let numberOfRounds;
+const buttons = document.querySelectorAll('button');
+const roundNumbers = document.querySelector('.roundNumbers');
+const compScore  = document.querySelector('.compScore');
+const playScore = document.querySelector('.playerScore');
+const compChoice = document.querySelector('.compChoice');
+const playChoice = document.querySelector('.playChoice');
+const roundMessage = document.querySelector('.roundMessage');
+const finalMessage = document.querySelector('.finalMessage');
+const replayModal = document.querySelector('.replayModal');
+const playAgainBtn = document.querySelector('#playAgainBtn');
+
+
+
+
+// let playerName;
 
 const gameChoices = ['Rock', 'Paper', 'Scissors']
 
@@ -8,82 +21,162 @@ let playerChoice;
 
 let computerchoice;
 
-let currentNumberOfRound;
+let currentNumberOfRound = 0;
 
-let computerScore;
-
-let playerScore;
-
-let restart = 'Yes'
-
-playerName =  prompt('What is your name? ');
+let numberOfRounds = Number(prompt('How many rounds do you want to play? '))
 
 
-//outer while loop to control the restart of the game
-while (restart === 'Yes') {
-    //restarts necessary variables if game restarted
+let computerScore = 0;
 
-    computerScore = 0;
-    playerScore = 0;
-    currentNumberOfRound = 0;
-    numberOfRounds = prompt('How many rounds?');
+let playerScore = 0;
+
+
+
+roundNumbers.textContent = numberOfRounds;
+compScore.textContent = 0;
+playScore.textContent = 0;
+
+
+
+function showReplayModal() {
+    replayModal.style.visibility = 'visible';
     
-      
-   //inner loop while to control a single gamee
-    while(currentNumberOfRound < numberOfRounds){
-        computerchoice = gameChoices[Math.floor(Math.random() * gameChoices.length )]
+}
+
+function hideReplayModal() {
+    replayModal.style.visibility= 'hidden';
+}
+
+
+
+
+buttons.forEach((btn)=>{
+    btn.classList ='btn';
+    btn.addEventListener('click', ()=>{
+        btn.classList.add('animate');
         
-         
-        playerChoice = prompt(`Pick one 'Rock', 'Paper' 'Scissors'`);
-        playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase()
-        console.log(playerChoice)
+        setTimeout(()=>{
+            btn.classList.remove('animate')
+        }, 200)
+    })
+
+})
+
+playAgainBtn.addEventListener('click', ()=>{
     
+})
+
+
+buttons.forEach((btn)=>{
+     btn.addEventListener('click', ()=>{
+        playerChoice = btn.id;
+        playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+
+        computerchoice = gameChoices[Math.floor(Math.random() * gameChoices.length )];
+
+        playChoice.textContent = playerChoice;
+        compChoice.textContent = computerchoice;
+
+        
+
         if(playerChoice === computerchoice){
-    
-            console.log(`It's a tie!`)
-            console.log(`Computer choice: ${computerchoice} \n Player choice : ${playerChoice}`)
-            console.log(`Computer Score : ${computerScore} \n ${playerName} : ${playerScore}`)
+            roundMessage.textContent= `It's a tie!`;
             numberOfRounds = numberOfRounds - 1;
-            console.log(`number of rounds remaining : ${numberOfRounds}`)
-    
-        }else if (playerChoice == 'Rock' && computerchoice == 'Paper'
-            || playerChoice == 'Paper' && computerchoice == 'Scissors' 
-            || computerchoice == 'Rock' && playerChoice == 'Scissors' ){
-            console.log('You loose!')
-            console.log(`Computer choice: ${computerchoice} \n Player choice : ${playerChoice}`)
-            computerScore += 1;
-            console.log(`Computer Score : ${computerScore} \n ${playerName} : ${playerScore}`)
-            numberOfRounds = numberOfRounds - 1;
-            console.log(`number of rounds remaining : ${numberOfRounds}`)
-    
-        }else if(playerChoice == 'Paper' && computerchoice == 'Rock' 
-            || playerChoice == 'Scissors' && computerchoice == 'Paper'
-            || playerChoice == 'Rock' && computerchoice == 'Scissors'){
-            console.log('You win!')
-            console.log(`Computer choice: ${computerchoice} \n Player choice : ${playerChoice}`)
-            playerScore += 1;
-            console.log(`Computer Score : ${computerScore} \n ${playerName} : ${playerScore}`)
-            numberOfRounds = numberOfRounds - 1;
-            console.log(`number of rounds remaining : ${numberOfRounds}`)
+            roundNumbers.textContent = numberOfRounds
+             }
+            
+            else if (playerChoice == 'Rock' && computerchoice == 'Paper'
+                || playerChoice == 'Paper' && computerchoice == 'Scissors' 
+                || computerchoice == 'Rock' && playerChoice == 'Scissors' ){
+                roundMessage.textContent = 'You loose!';
+              
+               
+                numberOfRounds = numberOfRounds - 1;
+                roundNumbers.textContent = numberOfRounds
+
+                computerScore = computerScore + 1;
+                compScore.textContent = computerScore;
+
+                
         
-        }
-        
-    }
+            }
+            else if(playerChoice == 'Paper' && computerchoice == 'Rock' 
+                || playerChoice == 'Scissors' && computerchoice == 'Paper'
+                || playerChoice == 'Rock' && computerchoice == 'Scissors'){
+                roundMessage.textContent = 'You win!';
+                
 
-    restart = prompt(`Do you want to restart the game? 'Yes', 'No'`)
-    restart = restart.charAt(0).toUpperCase() + restart.slice(1).toLowerCase();
-    
-}
+                numberOfRounds = numberOfRounds - 1;
+                roundNumbers.textContent = numberOfRounds
 
+                playerScore = playerScore + 1;
+                playScore.textContent = playerScore;
 
+            
+            }
+            
+            if(numberOfRounds == 0){
+                if(playerScore > computerScore){
+                        finalMessage.textContent= 'Hurray you win the game!🥳'
+                    }else if (playerScore === computerScore){
+                        finalMessage.textContent = 'You tied! Try to get a better edge next time😂'
+                    }else {
+                        finalMessage.textContent = 'Oops, You loose!😭'
+                    }
+            }
+            if(numberOfRounds < 0){
+            
+               
+               
+                let restart = confirm('Do you want to restart the game? ')
 
-
-
-if(playerScore > computerScore){
-    console.log('Hurray you win the game!🥳')
-}else if (playerScore === computerScore){
-    console.log('You tied! Try to get a better edge next time😂')
-}else {
-    console.log('Oops, You loose!😭')
-}
+                
+                if (restart) {
+                   
+                    numberOfRounds = prompt('How many rounds do you want to play? ');
+                    roundNumbers.textContent = numberOfRounds;
+                    finalMessage.textContent = '';
+                    compScore.textContent = 0;
+                    playScore.textContent = 0;
+                    playChoice.textContent = '';
+                    compChoice.textContent = '';
+                    roundMessage.textContent = '';
+                    computerScore = 0;
+                    playerScore = 0;
+                    currentNumberOfRound = 0;
+                }else{
+                    
+                    numberOfRounds = 0;
+                    roundNumbers.textContent = numberOfRounds;
+                    finalMessage.textContent = '';
+                    compScore.textContent = 0;
+                    playScore.textContent = 0;
+                    playChoice.textContent = '';
+                    compChoice.textContent = '';
+                    roundMessage.textContent = '';
+                    computerScore = 0;
+                    playerScore = 0;
+                    currentNumberOfRound = 0;
+                }
+                
+            }else if(numberOfRounds > 20){
+                numberOfRounds = 0;
+                roundNumbers.textContent = 0;
+                finalMessage.textContent = '';
+                compScore.textContent = 0;
+                playScore.textContent = 0;
+                playChoice.textContent = '';
+                compChoice.textContent = '';
+                roundMessage.textContent = '';
+                computerScore = 0;
+                playerScore = 0;
+                currentNumberOfRound = 0;
+                alert('Pity yourself that is much!😭')
+                    
+            }
+            
+                        
+  
+    })
+})
 
